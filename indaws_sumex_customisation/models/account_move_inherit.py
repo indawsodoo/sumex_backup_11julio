73b13237fe88374103c93db7e3779f8255a467f2
MitalAktiv <mital@aktivsoftware.com>
2022-05-18 16:27:57 +0530

    [MOD] # | indaws_sumex_customisation | added the new method  for the script

--- indaws_sumex_customisation/models/account_move_inherit.py
+++ indaws_sumex_customisation/models/account_move_inherit.py
@@ -117,7 +117,7 @@ class AccountMoveLineInherit(models.Model):
 
     quantity_2 = fields.Float(string='quantity 2')
 
-    def account_move_line_cron_job_custom_indaws_live(self):
+    def account_move_line_cron_job_custom_indaws_livess(self):
         server = '10.210.86.100'
         database = 'sage'
         username = 'consultasit'
@@ -202,7 +202,7 @@ class AccountMoveLineInherit(models.Model):
             except Exception as e:
                 print(e)
 
-    def account_move_line_cron_job_custom_indaws_stage(self):
+    def account_move_line_cron_job_custom_indaws_stagess(self):
         server = '10.210.86.100'
         database = 'sage'
         username = 'consultasit'
